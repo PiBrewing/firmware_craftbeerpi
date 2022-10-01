2202d3dbd2f7054c0360e46b6609b478b6fc1621
Till Klocke <till.klocke@gmail.com>
2022-10-01 16:22:59 +0200

    Added dependencies from original requirements.txt for craftbeerpi

--- package/craftbeerpi/craftbeerpi.mk
+++ package/craftbeerpi/craftbeerpi.mk
@@ -5,6 +5,14 @@ CRAFTBEERPI_SITE = $(call github,craftbeerpi,craftbeerpi4,$(CRAFTBEERPI_VERSION)
 CRAFTBEERPI_LICENSE = GPL-3
 CRAFTBEERPI_LICENSE_FILES = LICENSE
 CRAFTBEERPI_SETUP_TYPE = setuptools
+CRAFTBEERPI_DEPENDENCIES = python-requests python-aiohttp \
+	python-aiohttp-auth python-aiohttp-route-decorator \
+	python-aiohttp-security python-aiohttp-session python-aiohttp-swagger \
+	python-aiojobs python-aiosqlite python-cryptography python-voluptuous \
+	python-pyfiglet python-pandas python-shortuuid python-tabulate python-numpy \
+	python-click python-importlib_metadata python-asyncio-mqtt \
+	python-psutil python-zipp python-pyinquirer python-colorama python-pytest-aiohttp \
+	python-coverage craftbeerpi-ui
 
 define CRAFTBEERPI_INSTALL_INIT_SYSTEMD
 	$(INSTALL) -D -m 644 -t $(TARGET_DIR)/usr/lib/systemd/system $(CRAFTBEERPI_PKGDIR)/craftbeerpi.service
