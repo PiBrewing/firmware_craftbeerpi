c6a4c1f357dde3cecf9095f9d34b956fddd19f36
Till Klocke <till.klocke@gmail.com>
2022-10-02 12:22:53 +0200

    Pandas 1.5.0 can't be build with the cython version provided by buildroot

--- package/python-pandas/python-pandas.mk
+++ package/python-pandas/python-pandas.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_PANDAS_VERSION = 1.5.0
+PYTHON_PANDAS_VERSION = 1.4.1
 PYTHON_PANDAS_SOURCE = pandas-$(PYTHON_PANDAS_VERSION).tar.gz
-PYTHON_PANDAS_SITE = https://files.pythonhosted.org/packages/2a/24/f5042daa59b91e94e6ea41edbb28d2b7e3712d0cf54a76f9ffde394efbe7
+PYTHON_PANDAS_SITE = https://files.pythonhosted.org/packages/c4/eb/cfa96ba42695b3c28d4864a796d492f188471dd536df7e5e5e0c54b629a6
 PYTHON_PANDAS_SETUP_TYPE = setuptools
 PYTHON_PANDAS_LICENSE = BSD 3-Clause License
 PYTHON_PANDAS_LICENSE_FILES = LICENSE
+PYTHON_PANDAS_DEPENDENCIES = python-numpy host-python-numpy
 
 $(eval $(python-package))
