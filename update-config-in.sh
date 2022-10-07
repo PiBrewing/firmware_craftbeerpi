084b86dda97e1745cdcd2e4b80bd6d6c0e440c71
Till Klocke <till.klocke@gmail.com>
2022-10-07 09:28:08 +0200

    Added util script to easily generate top Config.in file

---
+++ update-config-in.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -f Config.in
+touch Config.in
+for fp in package/* 
+do
+    PACKAGE_NAME=$(basename $fp)
+    CONFIG_LINE="source \"\$BR2_EXTERNAL_CRAFTBEERPI_PATH/${PACKAGE_NAME}/Config.in\""
+    echo "Adding ${CONFIG_LINE} to Config.in"
+    echo "$CONFIG_LINE" >> Config.in
+done
