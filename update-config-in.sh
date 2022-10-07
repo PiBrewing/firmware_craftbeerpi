197b80488a2fe94e17b63a91608c3a97285f1236
Till Klocke <till.klocke@gmail.com>
2022-10-07 09:29:53 +0200

    Fixed path written to global Config.in for each package

--- update-config-in.sh
+++ update-config-in.sh
@@ -5,7 +5,7 @@ touch Config.in
 for fp in package/* 
 do
     PACKAGE_NAME=$(basename $fp)
-    CONFIG_LINE="source \"\$BR2_EXTERNAL_CRAFTBEERPI_PATH/${PACKAGE_NAME}/Config.in\""
+    CONFIG_LINE="source \"\$BR2_EXTERNAL_CRAFTBEERPI_PATH/package/${PACKAGE_NAME}/Config.in\""
     echo "Adding ${CONFIG_LINE} to Config.in"
     echo "$CONFIG_LINE" >> Config.in
 done
