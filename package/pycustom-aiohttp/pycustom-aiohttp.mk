ea5904905d3c991a7ce5b71a2846dc0b5a49b2ca
Till Klocke <till.klocke@gmail.com>
2022-10-11 14:12:00 +0200

    Aiohttp was in the wrong version

--- package/pycustom-aiohttp/pycustom-aiohttp.mk
+++ package/pycustom-aiohttp/pycustom-aiohttp.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYCUSTOM_AIOHTTP_VERSION = 3.8.3
+PYCUSTOM_AIOHTTP_VERSION = 3.8.1
 PYCUSTOM_AIOHTTP_SOURCE = aiohttp-$(PYCUSTOM_AIOHTTP_VERSION).tar.gz
-PYCUSTOM_AIOHTTP_SITE = https://files.pythonhosted.org/packages/ff/4f/62d9859b7d4e6dc32feda67815c5f5ab4421e6909e48cbc970b6a40d60b7
+PYCUSTOM_AIOHTTP_SITE = https://files.pythonhosted.org/packages/5a/86/5f63de7a202550269a617a5d57859a2961f3396ecd1739a70b92224766bc
 PYCUSTOM_AIOHTTP_SETUP_TYPE = setuptools
 PYCUSTOM_AIOHTTP_LICENSE = FIXME: license id couldn't be detected
 PYCUSTOM_AIOHTTP_LICENSE_FILES = LICENSE.txt
