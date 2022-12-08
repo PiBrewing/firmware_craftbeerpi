4767ea3abe23224a6d2b80ee16f4cd6f7fb14520
Till Klocke <till.klocke@gmail.com>
2022-12-08 18:17:23 +0100

    We have now weston build and running, cog build but not running. There are quite a few things missing until we get to the point where get a browser in kiosk mode running

--- board/common/post-build.sh
+++ board/common/post-build.sh
@@ -8,6 +8,8 @@ mkdir -p ${TARGET_DIR}/boot
 rm -rf ${TARGET_DIR}/usr/share/factory/var
 cp -dpfr ${TARGET_DIR}/var ${TARGET_DIR}/usr/share/factory/
 
+cp ${TARGET_DIR}/usr/lib/libWPEBackend-fdo-1.0.so.1.8.0 ${TARGET_DIR}/usr/lib/libWPEBackend-default.so
+
 cat << EOF > ${TARGET_DIR}/etc/os-release
 NAME=craftbeerpi
 ID=craftbeerpi
