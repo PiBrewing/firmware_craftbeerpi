099fa2f0c2ada25cc39c1d429de524fa2cf7bf79
Till Klocke <till.klocke@gmail.com>
2023-01-26 15:25:42 +0100

    raucgithub can now successfully be build

--- package/raucgithub/raucgithub.mk
+++ package/raucgithub/raucgithub.mk
@@ -1,17 +1,19 @@
 RAUCGITHUB_VERSION = v0.1.0-pre.1
-RAUCGITHUB_SITE = $(call github,dereulenspiegel,raucgithub,$(RAUCGITHUB_VERSION))
-RAUCGITHUB_LICENSE = BSD-3-Clause
+RAUCGITHUB_SITE = $(call github,dereulenspiegel,rauc-github,$(RAUCGITHUB_VERSION))
+RAUCGITHUB_GOMOD = github.com/dereulenspiegel/raucgithub
+RAUCGITHUB_LICENSE = MIT
 RAUCGITHUB_LICENSE_FILES = LICENSE
-RAUCGITHUB_LDFLAGS = -s -w -X main.version=$(RAUCGITHUB_VERSION) -X main.commit='unspecified' -X main.date='unspecified' -X main.builtBy=buildroot
-RAUCGITHUB_TARGETS = cmd
+RAUCGITHUB_LDFLAGS = -s -w -X 'main.version=$(RAUCGITHUB_VERSION)' -X 'main.commit=unspecified' -X 'main.date=unspecified' -X 'main.builtBy=buildroot'
+RAUCGITHUB_BUILD_TARGETS = cmd
 RAUCGITHUB_BIN_NAME = raucgithub
+RAUCGITHUB_INSTALL_BINS = raucgithub
 
 define RAUCGITHUB_INSTALL_INIT_SYSTEMD
 	$(INSTALL) -D -m 644 -t $(TARGET_DIR)/usr/lib/systemd/system $(RAUCGITHUB_PKGDIR)/raucgithub.service
 endef
 
 define RAUCGITHUB_INSTALL_DBUS_POLICY
-	$(INSTALL) -D -m 644 -t $(TARGET_DIR)/etc/dbus-1/system.d/com.github.dereulenspiegel.rauc.conf $(RAUCGITHUB_PKGDIR)/dbus-policy.conf
+	$(INSTALL) -D -m 644 -t $(TARGET_DIR)/etc/dbus-1/system.d $(RAUCGITHUB_PKGDIR)/com.github.dereulenspiegel.rauc.conf
 endef
 
 RAUCGITHUB_POST_INSTALL_TARGET_HOOKS += RAUCGITHUB_INSTALL_DBUS_POLICY
