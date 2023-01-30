f6dafc42c93e0d348c08623c1b6b6aa83ee62deb
Till Klocke <till.klocke@gmail.com>
2023-01-30 18:31:02 +0100

    Seems rauc doesn't support ed25519, so we need to use oldschool RSA keys

--- .cert-staging/create-certs.sh
+++ .cert-staging/create-certs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 OPENSSL=/usr/local/opt/openssl@3/bin/openssl
 # Generate root key and CA cert
-$OPENSSL req -x509 -config openssl-ca.conf -days 10950 -newkey ed25519 -nodes -out ca.crt -outform PEM
+$OPENSSL req -x509 -config openssl-ca.conf -days 10950 -newkey rsa:4096 -sha256 -nodes -out ca.crt -outform PEM
 # Create CSR and private key for release certificate 
-$OPENSSL req -config signing-cert.conf -newkey ed25519 -nodes -out sign-release.csr -outform PEM
+$OPENSSL req -config signing-cert.conf -newkey rsa:4096 -sha256 -nodes -out sign-release.csr -outform PEM
 $OPENSSL ca -config openssl-ca.conf -policy signing_policy -extensions signing_req -out sign-release.crt -infiles sign-release.csr
